edc2bee2431567b66ba1e3cc9226fa895e658d3b
Md. Shihab <shihabrayhan8@gmail.com>
2021-09-21 23:14:16 +0600

    Program completed!

--- PrimeNumner.cs
+++ PrimeNumner.cs
@@ -4,6 +4,23 @@ public class PrimeNumber
 {
 	public PrimeNumber()
 	{
-
+		public static void Main(string[] agrs)
+        {
+			int n, i, m = 0, flag = 0;
+			Console.Write("Enter the number to check prime: ");
+			n = int.Parse(Console.ReadLine());
+			m = n / 2;
+			for(i=0; i<=m; i++)
+            {
+                if (n % 2 == 0)
+                {
+					Console.Write("Number is not prime.");
+					flag = 1;
+					break;
+                }
+            }
+			if (flag == 0)
+				Console.Write("Number is prime.");
+        }
 	}
 }
